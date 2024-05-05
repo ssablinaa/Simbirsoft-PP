709f9ea8a90f03a575ccac0fce0a461ef82edfd6
ssablinaa <160714722+ssablinaa@users.noreply.github.com>
2024-05-06 00:49:56 +0400

    Боковая панель меню

--- src/app/components/UI/header/header.component.ts
+++ src/app/components/UI/header/header.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/UI/header/header.component';
 import { FooterComponent } from '../../components/UI/footer/footer.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
