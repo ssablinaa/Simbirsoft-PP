974a75d9926da41206c94836112006e2fc75b813
ssablinaa <160714722+ssablinaa@users.noreply.github.com>
2024-05-08 00:19:46 +0400

    Главная страница

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/UI/header/header.component';
 import { FooterComponent } from '../../components/UI/footer/footer.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, MatCardModule],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
