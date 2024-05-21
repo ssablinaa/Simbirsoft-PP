c3328143cc457ba342d238420afb0572bbd07358
ssablinaa <160714722+ssablinaa@users.noreply.github.com>
2024-05-22 00:42:10 +0400

    Исправление маршрутизации

--- src/app/components/UI/header/header.routes.ts
+++ src/app/components/UI/header/header.routes.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../../../pages/home/home.component';
 export const routes: Routes = [
   { path: 'art', component: HomeComponent },
+  { path: 'music', component: HomeComponent },
   { path: 'dance', component: HomeComponent },
   { path: 'sport', component: HomeComponent },
   { path: 'games', component: HomeComponent },
@@ -10,7 +11,7 @@ export const routes: Routes = [
   { path: 'comics', component: HomeComponent },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class HeaderRoutes {}
